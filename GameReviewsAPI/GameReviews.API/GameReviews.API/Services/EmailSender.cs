bf466c0c3e899882d9cbfb4db2c89261f750cdbb
DanielMonoranu <monoranu_dani@yahoo.ro>
2023-06-24 00:01:24 +0300

    modified the email sender service

--- GameReviewsAPI/GameReviews.API/GameReviews.API/Services/EmailSender.cs
+++ GameReviewsAPI/GameReviews.API/GameReviews.API/Services/EmailSender.cs
@@ -1,52 +1,48 @@
 ﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestPlatform.Utilities.Helpers;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+using System.Net;
+using System.Net.Mail;
 
 namespace GameReviews.API.Services
 {
     public class EmailSender : IEmailSender
     {
-        public readonly string SendGridSecret;
+        public readonly string password;
         public EmailSender(IConfiguration configuration)
         {
-            SendGridSecret = configuration.GetConnectionString("SendGridApiKey");
-
+            password = configuration.GetConnectionString("EmailPassword");
         }
-
 
         public async Task<string> SendEmailAsync(string email, string text, List<string> adminsAccounts, IFormFile? picture)
         {
-            var apiKey = SendGridSecret;
-            var client = new SendGridClient(apiKey);
-
-            var from = new EmailAddress("monoranudani@gmail.com", "Starry Reviews");
-
             try
             {
-                foreach (var adminEmail in adminsAccounts)
+                var client = new SmtpClient("smtp.gmail.com", 587)
                 {
-                    var to = new EmailAddress(adminEmail, "Admin");
-                    var subject = "Critic Account";
-                    var plainTextContent = "Requesting Critic Account";
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("starryreviewsCo@gmail.com", password)
+                };
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("starryreviewsCo@gmail.com");
+                message.Subject = "Critic account";
+                message.IsBodyHtml = true;
+                if (picture != null)
+                {
+                    Stream fileStream = picture.OpenReadStream();
+                    Attachment attachment = new Attachment(fileStream, "useridentication.jpg");
+                    message.Attachments.Add(attachment);
+                }
+
+                foreach (var adminAccount in adminsAccounts)
+                {
                     var htmlContent = $"<strong>User {email} is requesting Critic account.Their message:</strong> <br><br>" + text; ;
-
-                    var message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-
-                    if (picture != null)
-                    {
-                        var attachment = new Attachment();
-                        var photoContent = ConvertImageToBase64(picture);
-                        attachment.Content = photoContent;
-                        attachment.Type = "image/jpeg";
-                        attachment.Filename = "userIdentication";
-                        message.AddAttachment(attachment);
-                    }
-
-                    var response = await client.SendEmailAsync(message);
-
+                    message.To.Add(new MailAddress(adminAccount));
+                    message.Body = htmlContent;
+                    await client.SendMailAsync(message);
                 }
                 return "Email sent succesfully";
             }
@@ -55,17 +51,5 @@ namespace GameReviews.API.Services
                 return ex.Message;
             }
         }
-
-        public string ConvertImageToBase64(IFormFile picture)
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                picture.CopyTo(memoryStream);
-                byte[] fileBytes = memoryStream.ToArray();
-                string base64String = Convert.ToBase64String(fileBytes);
-                return base64String;
-            }
-        }
-
     }
 }
--- GameReviewsAPI/GameReviews.API/GameReviews.API/Services/IEmailSender.cs
+++ GameReviewsAPI/GameReviews.API/GameReviews.API/Services/IEmailSender.cs
@@ -5,7 +5,5 @@ namespace GameReviews.API.Services
     public interface IEmailSender
     {
         public Task<string> SendEmailAsync(string email, string text, List<string> adminsAccounts, IFormFile? picture);
-
-        public string ConvertImageToBase64(IFormFile picture);
     }
 }
